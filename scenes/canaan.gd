9161bbce9598a9351d107a39183c6221ec00bb02
Chase Williamson <0chasew0@gmail.com>
2024-11-11 21:50:02 -0700

    camera zoom/pan

--- scenes/canaan.gd
+++ scenes/canaan.gd
@@ -1,15 +1,22 @@
 extends Node2D
 
-@onready var standard_map = $CanvasLayer/StandardMap
+@onready var standard_map = $MapLayer/StandardMap
 
 func _ready() -> void:
 	randomize() # Initializes randomizer, only call this once
-	generate_rand_standard_map()
+	var map_data = generate_rand_standard_map() # Map data contains coordinates of all cells of the map
+	build_road(map_data)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 	#generate_rand_standard_map()
 	#delta = delta / 12
+	
+func build_road(map_data):
+	
+	var a_cell = map_data[0]
+	pass
+	
 	
 func roll_dice() -> int:
 	return randi_range(1,6)
@@ -22,53 +29,52 @@ func _on_roll_dice_pressed() -> void:
 	else:
 		$CanvasLayer/Roll_Dice.text = "Rolled a " + str(result_1 + result_2) + " (" + str(result_1) + "," + str(result_2) + ")"
 
-func generate_rand_standard_map():
-	# Debug
-	while true:
-		var NUM_TREE_TILES = 4
-		var NUM_SHEEP_TILES = 4
-		var NUM_BRICK_TILES = 3
-		var NUM_WHEAT_TILES = 4
-		var NUM_STONE_TILES = 3
-		var NUM_DESERT_TILES = 1
+func generate_rand_standard_map() -> Array[Vector2i]:
+
+	var NUM_TREE_TILES = 4
+	var NUM_SHEEP_TILES = 4
+	var NUM_BRICK_TILES = 3
+	var NUM_WHEAT_TILES = 4
+	var NUM_STONE_TILES = 3
+	var NUM_DESERT_TILES = 1
+	
+	var TOTAL_NUM_TILES = 19
+	
+	var possible_placements: Array[Vector2i] = [
+		Vector2i(-2, -3), Vector2i(-1, -3), Vector2i(0, -3),
+		Vector2i(-2, -2), Vector2i(-1, -2), Vector2i(0, -2), Vector2i(1, -2),
+		Vector2i(-3, -1), Vector2i(-2, -1), Vector2i(-1, -1), Vector2i(0, -1), Vector2i(1, -1),
+		Vector2i(-2, 0), Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0),
+		Vector2i(-2, 1), Vector2i(-1, 1), Vector2i(0, 1)
+	]
+	
+	# Place tiles randomly, removing the selections from the list of possible placements each time
+	for i in range(NUM_TREE_TILES):
+		var random_placement = randi_range(0, len(possible_placements) - 1)
+		standard_map.set_cell(possible_placements[random_placement], 1, Vector2i(0,0))
+		possible_placements.pop_at(random_placement)
+	print(possible_placements)
+	for i in range(NUM_SHEEP_TILES):
+		var random_placement = randi_range(0, len(possible_placements) - 1)
+		standard_map.set_cell(possible_placements[random_placement], 2, Vector2i(0,0))
+		possible_placements.pop_at(random_placement)
+	print(possible_placements)
+	for i in range(NUM_BRICK_TILES):
+		var random_placement = randi_range(0, len(possible_placements) - 1)
+		standard_map.set_cell(possible_placements[random_placement], 3, Vector2i(0,0))
+		possible_placements.pop_at(random_placement)
+	print(possible_placements)
+	for i in range(NUM_WHEAT_TILES):
+		var random_placement = randi_range(0, len(possible_placements) - 1)
+		standard_map.set_cell(possible_placements[random_placement], 4, Vector2i(0,0))
+		possible_placements.pop_at(random_placement)
+	print(possible_placements)
+	for i in range(NUM_STONE_TILES):
+		var random_placement = randi_range(0, len(possible_placements) - 1)
+		standard_map.set_cell(possible_placements[random_placement], 5, Vector2i(0,0))
+		possible_placements.pop_at(random_placement)
 		
-		var TOTAL_NUM_TILES = 19
-		
-		var possible_placements: Array[Vector2i] = [
-			Vector2i(-2, -3), Vector2i(-1, -3), Vector2i(0, -3),
-			Vector2i(-2, -2), Vector2i(-1, -2), Vector2i(0, -2), Vector2i(1, -2),
-			Vector2i(-3, -1), Vector2i(-2, -1), Vector2i(-1, -1), Vector2i(0, -1), Vector2i(1, -1),
-			Vector2i(-2, 0), Vector2i(-1, 0), Vector2i(0, 0), Vector2i(1, 0),
-			Vector2i(-2, 1), Vector2i(-1, 1), Vector2i(0, 1)
-		]
-		
-		# Place tiles randomly, removing the selections from the list of possible placements each time
-		for i in range(NUM_TREE_TILES):
-			var random_placement = randi_range(0, len(possible_placements) - 1)
-			standard_map.set_cell(possible_placements[random_placement], 1, Vector2i(0,0))
-			possible_placements.pop_at(random_placement)
-		print(possible_placements)
-		for i in range(NUM_SHEEP_TILES):
-			var random_placement = randi_range(0, len(possible_placements) - 1)
-			standard_map.set_cell(possible_placements[random_placement], 2, Vector2i(0,0))
-			possible_placements.pop_at(random_placement)
-		print(possible_placements)
-		for i in range(NUM_BRICK_TILES):
-			var random_placement = randi_range(0, len(possible_placements) - 1)
-			standard_map.set_cell(possible_placements[random_placement], 3, Vector2i(0,0))
-			possible_placements.pop_at(random_placement)
-		print(possible_placements)
-		for i in range(NUM_WHEAT_TILES):
-			var random_placement = randi_range(0, len(possible_placements) - 1)
-			standard_map.set_cell(possible_placements[random_placement], 4, Vector2i(0,0))
-			possible_placements.pop_at(random_placement)
-		print(possible_placements)
-		for i in range(NUM_STONE_TILES):
-			var random_placement = randi_range(0, len(possible_placements) - 1)
-			standard_map.set_cell(possible_placements[random_placement], 5, Vector2i(0,0))
-			possible_placements.pop_at(random_placement)
-			
-		# Set desert tile
-		standard_map.set_cell(possible_placements[0], 6, Vector2i(0,0))
-		
-		await get_tree().create_timer(1.0).timeout
+	# Set desert tile
+	standard_map.set_cell(possible_placements[0], 6, Vector2i(0,0))
+	
+	return possible_placements
