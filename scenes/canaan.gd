711ca44cfb534dcd6c6b8cfae484aa5166191e10
Chase Williamson <0chasew0@gmail.com>
2025-02-12 17:20:27 -0700

    bots can place settlements and road.
    next: follow correct logic for settlements/roads

--- scenes/canaan.gd
+++ scenes/canaan.gd
@@ -35,10 +35,14 @@ extends Node2D
 @onready var BOT_1_TURN_NUM = 2
 @onready var BOT_2_TURN_NUM = 3
 @onready var BOT_3_TURN_NUM = 4
+@onready var BOT_1_SETTLEMENTS = []
+@onready var BOT_2_SETTLEMENTS = []
+@onready var BOT_3_SETTLEMENTS = []
 @onready var ELIGIBLE_SETTLEMENT_VERTICES = [] # Contains a list of eligible vertices for settlement placement. This is shared between all players
 
 # Signals
 signal selection_finished # Used for returning control back to a main function after a user selects an action or they timeout from that action
+signal end_turn # Used for signaling that a player or bots turn is over, usually used in the main game loop
 
 func _ready() -> void:
 	randomize() # Initializes randomizer, only call this once
@@ -55,8 +59,15 @@ func _ready() -> void:
 	#while true:
 		#pass
 	await roll_for_who_goes_first()
-	await place_initial_settlements_and_roads(GLOBAL_TURN_NUM)
-	await place_initial_settlements_and_roads(GLOBAL_TURN_NUM)
+	for j in range(2):
+		for i in range(NUM_PLAYERS):
+			print("global turn num: ", GLOBAL_TURN_NUM)
+			await place_initial_settlements_and_roads(GLOBAL_TURN_NUM)
+			GLOBAL_TURN_NUM += 1
+			if GLOBAL_TURN_NUM > 4:
+				GLOBAL_TURN_NUM = 1
+				break
+	chat_log.append_text("[font_size=%s]\nAll players done placing settlements and roads." % chat_log_font_size)
 
 func add_player_to_game():
 	# Determine if player is bot or real
@@ -105,7 +116,6 @@ func roll_for_who_goes_first():
 		GLOBAL_TURN_NUM += 1
 		
 	turn_order.sort_custom(custom_sort_for_first_roll)
-	print(turn_order)
 	
 	# turn_order[i].y will be a players_id, so assign player turn nums from that
 	for i in range(len(turn_order)):
@@ -117,7 +127,6 @@ func roll_for_who_goes_first():
 			BOT_2_TURN_NUM = i+1
 		elif turn_order[i].y == BOT_3_TURN_NUM:
 			BOT_3_TURN_NUM = i+1
-	print(PLAYER_TURN_NUM)
 	
 	chat_log.append_text("\n")
 	for i in range(len(turn_order)):
@@ -166,46 +175,65 @@ func custom_sort_for_first_roll(a, b):
 
 # A client will only do this once when it is their turn, bot functionality is added here for testing
 func place_initial_settlements_and_roads(GLOBAL_TURN_NUM):
-	for node in get_tree().get_nodes_in_group("UI_settlement_buttons"):
-		node.show()
-	#if GLOBAL_TURN_NUM == PLAYER_TURN_NUM:
-		
 	# Update the chat log
 	var fmt_str = "[font_size=%s]\nPlayer %s place a settlement and road."
-	var act_str = fmt_str % [chat_log_font_size, PLAYER_NUM]
+	var act_str = fmt_str % [chat_log_font_size, GLOBAL_TURN_NUM]
 	chat_log.append_text(act_str)
 	
-	# Await a timer timeout -- timeout the timer in settlement_placement_pressed() when a settlement is chosen before the timer runs out.
-	# otherwise, if timer runs out before settlement is chosen, place settlement randomly for this player using GLOBAL_TURN_NUM
-	$MapLayer/Player1_Settlement_Timer.start()
-	await selection_finished # Wait for user input, else timer will timeout and do the same stuff as below
-	$MapLayer/Player1_Settlement_Timer.stop()
-	print("Back in function, place road now...")
-	
-	# Hide settlment_placement_buttons
-	for node in get_tree().get_nodes_in_group("UI_settlement_buttons"):
-		node.hide()
-	
-	# Place a road
-	# Roads in this init function will never branch from an existing road -- they will only extend from a settlement
-	# Player settlements array should hold latest settlement position -- always use last one
-	var settlement_pos = PLAYER_SETTLEMENTS.back()
-	init_possible_road_placements(settlement_pos)
-	await selection_finished
-	print("Done placing road.")
-	var i = 2
-	for node in get_node("MapLayer").get_children():
-		if node.name == "Possible_Placement_Road%s" % i:
-			i+=1
-			node.queue_free()
+	# If it is the client's turn and the client is not a bot
+	if GLOBAL_TURN_NUM == PLAYER_TURN_NUM:
+		print("player turn")
+		for node in get_tree().get_nodes_in_group("UI_settlement_buttons"):
+			node.show()
 		
-
-	#elif GLOBAL_TURN_NUM == BOT_1_TURN_NUM:
-		#pass
-	#elif GLOBAL_TURN_NUM == BOT_2_TURN_NUM:
-		#pass
-	#elif GLOBAL_TURN_NUM == BOT_3_TURN_NUM:
-		#pass
+		# Await a timer timeout -- timeout the timer in settlement_placement_pressed() when a settlement is chosen before the timer runs out.
+		# otherwise, if timer runs out before settlement is chosen, place settlement randomly for this player using GLOBAL_TURN_NUM
+		$MapLayer/Player1_Settlement_Timer.start()
+		await selection_finished # Wait for user input, else timer will timeout and do the same stuff as below
+		$MapLayer/Player1_Settlement_Timer.stop()
+		print("Back in function, place road now...")
+		
+		# Hide settlment_placement_buttons
+		for node in get_tree().get_nodes_in_group("UI_settlement_buttons"):
+			node.hide()
+		
+		# Place a road
+		# Roads in this init function will never branch from an existing road -- they will only extend from a settlement
+		# Player settlements array should hold latest settlement position -- always use last one
+		var settlement_pos = PLAYER_SETTLEMENTS.back()
+		init_possible_road_placements(settlement_pos)
+		await selection_finished
+		print("Done placing road.")
+		var i = 2
+		for node in get_node("MapLayer").get_children():
+			if node.name == "Possible_Placement_Road%s" % i:
+				i+=1
+				node.queue_free()
+		emit_signal("end_turn")
+		return
+		
+	# If the client is a bot
+	elif GLOBAL_TURN_NUM == BOT_1_TURN_NUM:
+		print("Bot 1 turn")
+		await bot_place_initial_settlement(GLOBAL_TURN_NUM, BOT_1_SETTLEMENTS)
+		var settlement_pos = BOT_1_SETTLEMENTS.back()
+		await bot_place_initial_road(settlement_pos, GLOBAL_TURN_NUM)
+		print("bot 1 turn done")
+		emit_signal("end_turn")
+	elif GLOBAL_TURN_NUM == BOT_2_TURN_NUM:
+		print("Bot 2 turn")
+		await bot_place_initial_settlement(GLOBAL_TURN_NUM, BOT_2_SETTLEMENTS)
+		var settlement_pos = BOT_2_SETTLEMENTS.back()
+		await bot_place_initial_road(settlement_pos, GLOBAL_TURN_NUM)
+		print("bot 2 turn done")
+		emit_signal("end_turn")
+	elif GLOBAL_TURN_NUM == BOT_3_TURN_NUM:
+		print("Bot 3 turn")
+		await bot_place_initial_settlement(GLOBAL_TURN_NUM, BOT_3_SETTLEMENTS)
+		var settlement_pos = BOT_3_SETTLEMENTS.back()
+		await bot_place_initial_road(settlement_pos, GLOBAL_TURN_NUM)
+		print("bot 3 turn done")
+		emit_signal("end_turn")
 func init_possible_road_placements(settlement_pos) -> void:
 	# Distance around 70
 	# Given a single point -- find all possible road placements branching from it using distance formula
@@ -223,24 +251,49 @@ func init_possible_road_placements(settlement_pos) -> void:
 			$MapLayer.add_child(curr_UI_element, true)
 			curr_UI_element.show()
 			curr_UI_element.position = midpoint + road_ui_btn_offset
-			# Pass vertex here is the vertex that connects the settlement vertex to the next vertex
-			curr_UI_element.pressed.connect(road_placement_pressed.bind(curr_UI_element, GLOBAL_TURN_NUM, vertex))
+			# Passed vertex here is the vertex that connects the settlement vertex to the next vertex
+			curr_UI_element.pressed.connect(road_placement_pressed.bind(curr_UI_element, GLOBAL_TURN_NUM, vertex, settlement_pos))
 
-func road_placement_pressed(midpoint_btn_node, GLOBAL_TURN_NUM, connected_vertex):
+func road_placement_pressed(midpoint_btn_node, GLOBAL_TURN_NUM, connected_vertex, settlement_vertex):
 	
 	# When the midpoint button is pressed -- show a road between the two points and add those two points as a road to this player
 	var road_ui_offset = Vector2(-25, 7.5)
-	var road_ui_rotation = 30
 	var ui_element_for_road = $MapLayer/Player1_Road.duplicate()
 	$MapLayer.add_child(ui_element_for_road)
 	ui_element_for_road.show()
 	ui_element_for_road.pivot_offset = Vector2(ui_element_for_road.size.x / 2, ui_element_for_road.size.y / 2)
 	ui_element_for_road.position = midpoint_btn_node.position + road_ui_offset # Place road at midpoint then rotate
 	
-	# Use slope between two points to calculate how to rotate the UI element
-	ui_element_for_road.rotation_degrees = 30
+	# Use slope and arctan between two points to calculate how to rotate the UI element
+	var slope = ((connected_vertex.y - settlement_vertex.y) / (connected_vertex.x - settlement_vertex.x))
+	var degrees = rad_to_deg(atan(slope))
+	ui_element_for_road.rotation_degrees = degrees
 	
 	emit_signal("selection_finished")
+
+func bot_place_initial_road(settlement_pos, GLOBAL_TURN_NUM) -> void:
+	var eligible_road_placements = []
+	for vertex in ELIGIBLE_SETTLEMENT_VERTICES:
+		var distance = sqrt(((vertex.x - settlement_pos.x)**2) + ((vertex.y - settlement_pos.y)**2))
+		if distance > 50 and distance < 75:
+			# Find midpoint to place UI element
+			eligible_road_placements.append(vertex)
+	var rand_num = randi_range(0, len(eligible_road_placements)-1)
+	var chosen_road = eligible_road_placements[rand_num]
+	var midpoint = Vector2(((chosen_road.x + settlement_pos.x) / 2), ((chosen_road.y + settlement_pos.y) / 2))
+	
+	var road_ui_offset = Vector2(-25, 7.5)
+	var ui_element_for_road = $MapLayer/Player1_Road.duplicate()
+	$MapLayer.add_child(ui_element_for_road)
+	ui_element_for_road.show()
+	ui_element_for_road.pivot_offset = Vector2(ui_element_for_road.size.x / 2, ui_element_for_road.size.y / 2)
+	ui_element_for_road.position = midpoint + road_ui_offset # Place road at midpoint then rotate
+	
+	# Use slope and arctan between two points to calculate how to rotate the UI element
+	var slope = ((chosen_road.y - settlement_pos.y) / (chosen_road.x - settlement_pos.x))
+	var degrees = rad_to_deg(atan(slope))
+	ui_element_for_road.rotation_degrees = degrees
+	
 
 # Initialize settlement placement buttons group -- this is done once for each game!
 # Afterwards, make changes to the group
@@ -311,6 +364,31 @@ func _on_player_1_settlement_timer_timeout() -> void:
 	update_eligible_settlement_vertices(selected_node_position, selected_node)
 	
 	# Return to main function
+	emit_signal("selection_finished")
+
+# Use Global turn num to match texture to correct bot (player too?)
+func bot_place_initial_settlement(GLOBAL_TURN_NUM, BOT_SETTLEMENT_ARRAY) -> void:
+	var rand_index = randi_range(0, len(ELIGIBLE_SETTLEMENT_VERTICES))
+	# Add random settlement as UI element
+	var offset_position = Vector2(-15, -15)
+	var selected_node_position = ELIGIBLE_SETTLEMENT_VERTICES[rand_index]
+	var ui_element_for_selected_settlement = $MapLayer/Player1_Settlement.duplicate()
+	$MapLayer.add_child(ui_element_for_selected_settlement)
+	ui_element_for_selected_settlement.show()
+	ui_element_for_selected_settlement.position = selected_node_position + offset_position
+	
+	# Add settlement to bot
+	BOT_SETTLEMENT_ARRAY.append(selected_node_position)
+	
+	# Update eligible vertices
+	var selected_node
+	for node in get_node("MapLayer").get_children(): # node.position is offset
+		if "TextureButton" in node.name:
+			if node.position == (selected_node_position + Vector2(-12, -9)):
+				selected_node = node
+	
+	update_eligible_settlement_vertices(selected_node_position, selected_node)
+	
 	emit_signal("selection_finished")
 
 # Updates the global array for eligible vertices for settlement placement
